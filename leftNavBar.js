d9ff4ebd0b8e1e4a0e27e4220df6298342b11b19
work-tsolanki <work.tsolanki@gmail.com>
2025-07-26 23:07:56 +0530

    add LeftNavBar Styling Sheet File & JS File

---
+++ leftNavBar.js
@@ -0,0 +1,7 @@
+function dashboard_enter(){
+    document.getElementById("dashboard").style.boxShadow = "0px 0px 3px #81689D";
+}
+
+function dashboard_leave(){
+    document.getElementById("dashboard").style.boxShadow = "0px 0px 1px #81689D";
+}
