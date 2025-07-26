82fe4811f05da65656301d30c4bff078d7a69e7f
work-tsolanki <work.tsolanki@gmail.com>
2025-07-26 21:09:27 +0530

    Change module to Flask from PyQt5

--- WP.py
+++ WP.py
@@ -1,33 +1,19 @@
-from PyQt5 import QtWidgets, QtCore
-import sys
-import ctypes
+from flask import Flask 
 
 
 class Application():
 
     def __init__(self):
+        
+        self.flask_app = Flask(__name__)
 
-        self.app = QtWidgets.QApplication(sys.argv)
-        main_window = QtWidgets.QWidget()
-
-        #fetching Width x Height of Window
-        try:
-            user32 = ctypes.windll.user32
-            user32.SetProcessDPIAware() 
-            width = user32.GetSystemMetrics(0)  
-            height = user32.GetSystemMetrics(1)
-        except AttributeError:
-            QtWidgets.QMessageBox.warning(self.app, "Error", "Couldn't fetch resolution. Set to default")
-            main_window.setGeometry(0,0, 1920, 1080)
-        else:
-            main_window.setGeometry(0,0, width, height)
-            
-
+        @self.flask_app.route('/<name>')
+        def hello_world(name):
+            return f"Hello {name}"       
         
         
-        main_window.show()
-        self.app.exec_()
-        
-        
-        
-app = Application()
+if __name__ == '__main__':
+    
+    app = Application()
+    app.flask_app.run()
+    
