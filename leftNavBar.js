6007364df01d32003f3fda0e61e6599db921fc9c
work-tsolanki <work.tsolanki@gmail.com>
2025-07-27 18:56:06 +0530

    Complete leftNavBar

--- leftNavBar.js
+++ leftNavBar.js
@@ -1,7 +1,31 @@
-function dashboard_enter(){
-    document.getElementById("dashboard").style.boxShadow = "0px 0px 3px #81689D";
+function chats_enter(){
+    document.getElementById("chats").style.boxShadow = "0px 0px 3px #81689D";
 }
 
-function dashboard_leave(){
-    document.getElementById("dashboard").style.boxShadow = "0px 0px 1px #81689D";
+function chats_leave(){
+    document.getElementById("chats").style.boxShadow = "0px 0px 1px #81689D";
+}
+
+function channels_enter(){
+    document.getElementById("channels").style.boxShadow = "0px 0px 3px #81689D";
+}
+
+function channels_leave(){
+    document.getElementById("channels").style.boxShadow = "0px 0px 1px #81689D";
+}
+
+function settings_enter(){
+    document.getElementById("settings").style.boxShadow = "0px 0px 3px #81689D";
+}
+
+function settings_leave(){
+    document.getElementById("settings").style.boxShadow = "0px 0px 1px #81689D";
+}
+
+function profile_enter(){
+    document.getElementById("profile").style.boxShadow = "0px 0px 3px #81689D";
+}
+
+function profile_leave(){
+    document.getElementById("profile").style.boxShadow = "0px 0px 1px #81689D";
 }
