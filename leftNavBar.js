9238906c31554f7cb2bedb6aa659408e94109353
work-tsolanki <work.tsolanki@gmail.com>
2025-07-28 03:43:06 +0530

    change leftNavBar to sliding NavBar and add transition for smooth sliding

--- leftNavBar.js
+++ leftNavBar.js
@@ -1,31 +1,39 @@
 function chats_enter(){
-    document.getElementById("chats").style.boxShadow = "0px 0px 3px #81689D";
+    document.getElementById("chats").style.boxShadow = "0px 0px 5px #81689D";
+    document.querySelector(".leftNavBarBackground").style.width = "25vh";
 }
 
 function chats_leave(){
-    document.getElementById("chats").style.boxShadow = "0px 0px 1px #81689D";
+    document.getElementById("chats").style.boxShadow = "0px 0px 0px #81689D";
+    document.querySelector(".leftNavBarBackground").style.width = "10vh";
 }
 
 function channels_enter(){
-    document.getElementById("channels").style.boxShadow = "0px 0px 3px #81689D";
+    document.getElementById("channels").style.boxShadow = "0px 0px 5px #81689D";
+    document.querySelector(".leftNavBarBackground").style.width = "25vh";
 }
 
 function channels_leave(){
-    document.getElementById("channels").style.boxShadow = "0px 0px 1px #81689D";
+    document.getElementById("channels").style.boxShadow = "0px 0px 0px #81689D";
+    document.querySelector(".leftNavBarBackground").style.width = "10vh";
 }
 
 function settings_enter(){
-    document.getElementById("settings").style.boxShadow = "0px 0px 3px #81689D";
+    document.getElementById("settings").style.boxShadow = "0px 0px 5px #81689D";
+    document.querySelector(".leftNavBarBackground").style.width = "25vh";
 }
 
 function settings_leave(){
-    document.getElementById("settings").style.boxShadow = "0px 0px 1px #81689D";
+    document.getElementById("settings").style.boxShadow = "0px 0px 0px #81689D";
+    document.querySelector(".leftNavBarBackground").style.width = "10vh";
 }
 
 function profile_enter(){
-    document.getElementById("profile").style.boxShadow = "0px 0px 3px #81689D";
+    document.getElementById("profile").style.boxShadow = "0px 0px 5px #81689D";
+    document.querySelector(".leftNavBarBackground").style.width = "25vh";
 }
 
 function profile_leave(){
-    document.getElementById("profile").style.boxShadow = "0px 0px 1px #81689D";
+    document.getElementById("profile").style.boxShadow = "0px 0px 0px #81689D";
+    document.querySelector(".leftNavBarBackground").style.width = "10vh";
 }
