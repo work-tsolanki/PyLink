65ee71945de28cdfd6df50495b3258747271112b
work-tsolanki <work.tsolanki@gmail.com>
2025-07-28 18:36:42 +0530

    add newchat and dot_menu button

---
+++ appTemplates/js/contact_page.js
@@ -0,0 +1,7 @@
+function show_newchat_txt(){
+    document.querySelector(".show_newchat").style.opacity = "1";
+}
+
+function hide_newchat_txt(){
+    document.querySelector(".show_newchat").style.opacity = "0";
+}
