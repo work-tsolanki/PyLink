1593e3f4ce449a3ea32ac89a87303f231e122b3c
work-tsolanki <work.tsolanki@gmail.com>
2025-07-28 18:48:14 +0530

    add linear transition to leftNavBar text

--- appTemplates/js/contact_page.js
+++ appTemplates/js/contact_page.js
@@ -5,3 +5,11 @@ function show_newchat_txt(){
 function hide_newchat_txt(){
     document.querySelector(".show_newchat").style.opacity = "0";
 }
+
+function show_menu_txt(){
+    document.querySelector(".show_menu").style.opacity = "1";
+}
+
+function hide_menu_txt(){
+    document.querySelector(".show_menu").style.opacity = "0";
+}
