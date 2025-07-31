86c07c3dc2d84feaf590d300cda8f50b9729e18f
work-tsolanki <work.tsolanki@gmail.com>
2025-08-01 04:25:40 +0530

    add channel page

---
+++ appTemplates/js/channel_page.js
@@ -0,0 +1,7 @@
+function show_newchannel_txt(){
+    document.querySelector(".show_newchannel").style.display = "block";
+}
+
+function hide_newchannel_txt(){
+    document.querySelector(".show_newchannel").style.display = "none";
+}
